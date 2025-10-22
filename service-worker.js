4f9db8100252f2c84689dcc587a3c548bb9658d0
AnTaREEP-IITM <24f2007461@ds.study.iitm.ac.in>
2025-10-23 05:04:41 +0530

    Add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+// This is a service worker placeholder
+self.addEventListener('install', function(event) {
+    console.log('Service worker installed');
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('Service worker activated');
+});
